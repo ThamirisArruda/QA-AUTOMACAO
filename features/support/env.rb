6dd4d06fbf6c54b1e69cd937042190cb0eef74eb
Thamiris Arruda <95656461+ThamirisArruda@users.noreply.github.com>
2023-03-30 17:33:33 -0300

    add arquivos

---
+++ features/support/env.rb
@@ -0,0 +1,14 @@
+require 'capybara/cucumber'
+require 'capybara/rspec'
+require 'selenium-webdriver'
+require 'site_prism'
+require 'pry'
+
+CONFIG = YAML.load_file(File.dirname(__FILE__) + "/environments/#{ENVIRONMENT}. yml") 
+
+
+Capybara.configure do |config|
+    config.default_driver = selenium_chrome
+    config.app_host = CONFIG['url_default']
+    config.default_max_wait_time = 10
+end
