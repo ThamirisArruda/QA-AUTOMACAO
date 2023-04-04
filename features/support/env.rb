05258718fcedcd7777bb5bfdb87025c7f1b79e81
Thamiris Arruda <95656461+ThamirisArruda@users.noreply.github.com>
2023-04-04 11:57:17 -0300

    add arquivos

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,11 +4,14 @@ require 'selenium-webdriver'
 require 'site_prism'
 require 'pry'
 
+ENVIRONMENT = ENV['ENVIRONMENT']
+puts "Ambiente >> #{ENVIRONMENT}"
+
 CONFIG = YAML.load_file(File.dirname(__FILE__) + "/environments/#{ENVIRONMENT}. yml") 
 
 
 Capybara.configure do |config|
-    config.default_driver = selenium_chrome
+    config.default_driver = :selenium_chrome
     config.app_host = CONFIG['url_default']
     config.default_max_wait_time = 10
 end
